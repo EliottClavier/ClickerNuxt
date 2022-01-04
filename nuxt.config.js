6885749829e6b443274104b4b7515d9cfdf2ab2d
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-01-04 15:47:58 +0100

    Update nuxtconfig pour build dans le dir public

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  generate: {
+    dir: 'public'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - DeployNuxtB3',
